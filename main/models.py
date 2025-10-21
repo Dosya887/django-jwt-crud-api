700ffbfeabb5dccc539cbd79c374d852775e6e8d
Dosya887 <tdastan.777@mail.ru>
2025-10-21 14:36:45 +0600

    Initial Commit: Django API with JWT and CRUD

---
+++ main/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+    def __str__(self):
+        return self.title
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    price = models.FloatField()
+    description = models.TextField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.title
+
+
---
+++ main/views.py
@@ -0,0 +1,16 @@
+from rest_framework import viewsets, permissions
+from .models import Category, Product
+from .serializers import CategorySerializer, ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permissions_classes = [permissions.IsAuthenticatedOrReadOnly]
+
